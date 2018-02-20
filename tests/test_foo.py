1c88cf53e356456fe083abf5d483bd665b708cb8
Krishna V <krishnarules@gmail.com>
2018-02-20 18:34:17 -0500

    pytest

---
+++ tests/test_foo.py
@@ -0,0 +1,14 @@
+import pytest
+from src.foo import *
+
+def test_add():
+    assert add(1,2) == 3
+    assert add(0,0) == 0
+    assert add(3,-1) == 2
+    assert add(-1,3) == 2
+
+def test_sub():
+    assert sub(1,2) == -1
+    assert sub(0,0) == 0
+    assert sub(3,-1) == 4
+    assert sub(-1,3) == -4
