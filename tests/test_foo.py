6a08596f9f1758db0f58d3df4f8952624ef2b0f7
krishnavaidy <krishnarules@gmail.com>
2018-02-21 13:54:07 -0500

    Update test_foo.py

--- tests/test_foo.py
+++ tests/test_foo.py
@@ -8,7 +8,7 @@ def test_add():
     assert add(-1,3) == 2
 
 def test_sub():
-    assert sub(1,2) == -1
+    assert sub(1,2) == 0
     assert sub(0,0) == 0
     assert sub(3,-1) == 4
     assert sub(-1,3) == -4
